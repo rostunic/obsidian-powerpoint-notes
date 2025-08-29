e4883e3a6d36bb319049c0d7a41e18db36688c10
rostunic <39241552+rostunic@users.noreply.github.com>
2025-08-29 14:44:06 +0200

    Changed method

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { CachedMetadata, Editor, EditorPosition, Loc, MarkdownFileInfo, Notice, Plugin } from 'obsidian';
+import { CachedMetadata, Editor, EditorPosition, HeadingCache, ListItemCache, Loc, MarkdownFileInfo, Notice, Plugin, Pos } from 'obsidian';
 import { PowerPointFile } from './PowerPointFile';
 
 
@@ -10,9 +10,7 @@ export default class AliasPickerPlugin extends Plugin {
 			id: 'powerpoint-notes:write-notes-to-slides',
 			name: 'Write notes to slides',
 			editorCheckCallback: (checking: boolean, editor: Editor, markdownFileInfo: MarkdownFileInfo) => {
-				const file = markdownFileInfo.file;
-				if (!file)
-					return false;
+				const file = markdownFileInfo.file; if (!file) return false;
 
 				const fileCache = this.app.metadataCache.getFileCache(file);
 				if (!fileCache)
@@ -34,7 +32,7 @@ export default class AliasPickerPlugin extends Plugin {
 					new Notice('The specified PowerPoint file path must end with `.pptx`');
 					return;
 				}
-				async function writeToPowerPoint(headersWithBulletPoints: HeaderBulletPoints) {
+				async function writeToPowerPoint(headersWithBulletPoints: HeaderBulletPoint[]) {
 					const powerPointFile = await PowerPointFile.loadAsync(powerPointFilePath);
 					const modifiedPath = powerPointFilePath.replace('.pptx', `_modified.pptx`);
 					try {
@@ -76,27 +74,36 @@ function toEditorLineStartPosition(pos: Loc): EditorPosition {
 	return { line: pos.line, ch: 0 };
 }
 
-type HeaderBulletPoints = {
+type HeaderBulletPoint = {
 	header: string;
 	bulletPoints: string[];
-}[];
+};
 
-function getHeadersWithBulletPointsInFile(fileCache: CachedMetadata, editor: Editor): HeaderBulletPoints | undefined {
+function getHeadersWithBulletPointsInFile(fileCache: CachedMetadata, editor: Editor): HeaderBulletPoint[] | undefined {
 	const headers = fileCache.headings;
 	if (!headers || headers.length === 0) return [];
 	const allListItems = fileCache.listItems;
 	if (!allListItems || allListItems.length === 0) return [];
-	const result: { header: string, bulletPoints: string[] }[] = [];
-	for (const header of headers) {
-		const nextHeader = headers.find(h => h.position.start.line > header.position.start.line);
-		const listItems = allListItems.filter(item => item.position.start.line > header.position.start.line && item.position.start.line < (nextHeader?.position.start.line ?? Infinity));
 
-		const listItemTexts = listItems.map(item => {
-			const startPosition: EditorPosition = toEditorLineStartPosition(item.position.start);
-			const endPosition: EditorPosition = toEditorPosition(item.position.end);
-			return editor.getRange(startPosition, endPosition);
-		});
-		result.push({ header: header.heading, bulletPoints: listItemTexts });
-	}
-	return result;
+	return headers.map((header, index) => {
+		const startPosition = header.position.start.line;
+		const endPosition = headers.at(index + 1)?.position.start.line;
+		return {
+			header: header.heading,
+			bulletPoints: allListItems.filter(item => isBetweenLines(item, startPosition, endPosition))
+				.map(x => getText(x, editor))
+		};
+	});
+}
+
+function isBetweenLines(item: ListItemCache, startLine: number, endLine: number | undefined): boolean {
+	const itemLine = item.position.start.line;
+	return itemLine > startLine && itemLine < (endLine ?? Infinity);
+}
+
+
+function getText(item: ListItemCache, editor: Editor) {
+	const startPosition: EditorPosition = toEditorLineStartPosition(item.position.start);
+	const endPosition: EditorPosition = toEditorPosition(item.position.end);
+	return editor.getRange(startPosition, endPosition);
 }
