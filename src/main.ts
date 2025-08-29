bd8da1456cfb1bb5bae2dea008bfd9071d36d0e9
rostunic <39241552+rostunic@users.noreply.github.com>
2025-08-29 15:01:07 +0200

    Extract method

--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,52 @@
 import { CachedMetadata, Editor, EditorPosition, HeadingCache, ListItemCache, Loc, MarkdownFileInfo, Notice, Plugin, Pos } from 'obsidian';
 import { PowerPointFile } from './PowerPointFile';
 
+const propertyNamePowerPointFile = 'powerPoint-file';
 
 export default class AliasPickerPlugin extends Plugin {
 
 	async onload() {
-
 		this.addCommand({
 			id: 'powerpoint-notes:write-notes-to-slides',
 			name: 'Write notes to slides',
-			editorCheckCallback: (checking: boolean, editor: Editor, markdownFileInfo: MarkdownFileInfo) => {
-				const file = markdownFileInfo.file; if (!file) return false;
-
-				const fileCache = this.app.metadataCache.getFileCache(file);
-				if (!fileCache)
-					return false;
-
-				const headersWithBulletPoints = getHeadersWithBulletPointsInFile(fileCache, editor);
-				if (headersWithBulletPoints === undefined)
-					return false;
-				if (checking)
-					return true;
-
-				const frontmatter = fileCache.frontmatter;
-				const powerPointFilePath = frontmatter?.['powerPoint-file'];
-				if (!powerPointFilePath || typeof powerPointFilePath !== 'string') {
-					new Notice('You have to specify the path for your PowerPoint file in the frontmatter with `powerPoint-file: <path>.pptx`');
-					return;
-				}
-				if (!powerPointFilePath.endsWith('.pptx')) {
-					new Notice('The specified PowerPoint file path must end with `.pptx`');
-					return;
-				}
-				async function writeToPowerPoint(headersWithBulletPoints: HeaderBulletPoint[]) {
-					const powerPointFile = await PowerPointFile.loadAsync(powerPointFilePath);
-					const modifiedPath = powerPointFilePath.replace('.pptx', `_modified.pptx`);
-					try {
-						const clonedFile = await powerPointFile.copyAsync(modifiedPath);
-
-						for (let i = 0; i < headersWithBulletPoints.length; i++) {
-							const header = headersWithBulletPoints[i];
-							const bulletPoints = header.bulletPoints;
-
-							await clonedFile.writeNotesFileAsync(i + 1, bulletPoints);
-						}
-						await clonedFile.saveAsync();
-						new Notice(`Wrote notes to ${modifiedPath}`);
-					} catch (error) {
-						if (error instanceof Error) {
-							if ((error as any).code === "EBUSY") {
-								new Notice('Failed to save PowerPoint file. It is locked, so you probably need to close it.');
-								return;
-							}
-						}
-						new Notice('Failed to save PowerPoint file. Error: ' + error);
-					}
-				}
-				writeToPowerPoint(headersWithBulletPoints);
-			}
+			editorCheckCallback: writeNotesToSlides
 		});
-
 	}
 
 	onunload() {
 
 	}
+}
+
+
+function writeNotesToSlides(checking: boolean, editor: Editor, markdownFileInfo: MarkdownFileInfo) {
+	const checkResult = getWriteNotesToSlidesData(checking, editor, markdownFileInfo);
+	if (typeof checkResult === 'boolean') return checkResult;
+	if (checkResult instanceof Notice) return undefined;
+
+	writeToPowerPoint(checkResult.headersWithBulletPoints, checkResult.powerPointFilePath);
+}
+
+function getWriteNotesToSlidesData(checking: boolean, editor: Editor, markdownFileInfo: MarkdownFileInfo) {
+	const file = markdownFileInfo.file;
+	if (!file) return false;
+
+	const fileCache = markdownFileInfo.app.metadataCache.getFileCache(file);
+	if (!fileCache) return false;
+	if (checking) return true;
+
+	const headersWithBulletPoints = getHeadersWithBulletPointsInFile(fileCache, editor);
+	if (headersWithBulletPoints === undefined)
+		return new Notice('Failed to extract headers and bullet points.');
+
+	const frontmatter = fileCache.frontmatter;
+	const powerPointFilePath = frontmatter?.[propertyNamePowerPointFile];
+	if (!powerPointFilePath || typeof powerPointFilePath !== 'string')
+		return new Notice(`You have to specify the path for your PowerPoint file in the frontmatter with \`${propertyNamePowerPointFile}: <path>.pptx\``);
+	if (!powerPointFilePath.endsWith('.pptx'))
+		return new Notice('The specified PowerPoint file path must end with `.pptx`');
+
+	return { headersWithBulletPoints, powerPointFilePath };
 }
 
 function toEditorPosition(pos: Loc): EditorPosition {
@@ -78,6 +60,29 @@ type HeaderBulletPoint = {
 	header: string;
 	bulletPoints: string[];
 };
+
+async function writeToPowerPoint(headersWithBulletPoints: HeaderBulletPoint[], powerPointFilePath: string) {
+	const powerPointFile = await PowerPointFile.loadAsync(powerPointFilePath);
+	const modifiedPath = powerPointFilePath.replace('.pptx', `_modified.pptx`);
+	try {
+		const clonedFile = await powerPointFile.copyAsync(modifiedPath);
+
+		for (let i = 0; i < headersWithBulletPoints.length; i++) {
+			const header = headersWithBulletPoints[i];
+			const bulletPoints = header.bulletPoints;
+
+			await clonedFile.writeNotesFileAsync(i + 1, bulletPoints);
+		}
+		await clonedFile.saveAsync();
+		new Notice(`Wrote notes to ${modifiedPath}`);
+	} catch (error) {
+		if (error instanceof Error && (error as any).code === "EBUSY") {
+			new Notice('Failed to save PowerPoint file. It is locked, so you probably need to close it.');
+			return;
+		}
+		new Notice(`Failed to save PowerPoint file. Error: ${error}`);
+	}
+}
 
 function getHeadersWithBulletPointsInFile(fileCache: CachedMetadata, editor: Editor): HeaderBulletPoint[] | undefined {
 	const headers = fileCache.headings;
